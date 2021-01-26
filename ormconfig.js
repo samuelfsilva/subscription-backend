89f713a462772f69bcff6983e4f1a507ec76adde
samuelfsilva <samukfer@gmail.com>
2021-01-26 00:51:33 -0300

    Refatoração.

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
     "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
-    "username": "root",
-    "password": "",
-    "database": "subscription",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE_NAME,
     "synchronize": true,
     "logging": false,
     "migrations": [
